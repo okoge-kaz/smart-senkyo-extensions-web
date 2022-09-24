27ed212e6c2c4b729a8482c6b773214821e50669
gray-1to <graystin88@gmail.com>
2022-09-24 15:55:23 +0900

    delete templetes & add index content instead

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
-import { AddFileStep } from 'components/templates/AddFileStep';
-import { ChooseOptionEntrance } from 'components/templates/ChooseOptionEntrance';
+import { ToNextButton } from 'components/atoms/ToNextButton';
+import { UploadButton } from 'components/atoms/UploadButton';
+import { Header } from 'components/organisms/Hreader';
+import { MainContent } from 'components/organisms/MainContent';
 import type { NextPage } from 'next';
 import { ReactElement, useState } from 'react';
 
 const Home: NextPage = () => {
+  // ファイル管理
+  const [file, setFile] = useState<File|null>(null)
+  
   // ステップ管理
   const [step, setStep] = useState<number>(1);
   const proceedStep = () => {
@@ -14,27 +19,31 @@ const Home: NextPage = () => {
     }
   }
 
+  // 1番目のステップのメインのボタン
+  const add_file_button: ReactElement = <UploadButton label="Add File" onActed={proceedStep} setFile={setFile}/> 
+  // 2番目のステップのメインのボタン
+  const to_option_choice_button: ReactElement = <ToNextButton label="Choose Option" onClick={proceedStep}/>
+
   const display_content = (step: number): ReactElement => {
     switch (step){
       case 1:
-        return <AddFileStep setFile={setFile} onActed={proceedStep}/>
+        return  <MainContent main_action_direction="名簿整形したいファイルを選択してください shiftを押しながらで複数選択できます" main_button_elements={add_file_button}/>
       case 2:
-        return <ChooseOptionEntrance onActed={proceedStep}/>
-      case 3:
-        return 
+        return <MainContent main_action_direction="オプションを選択してください" main_button_elements={to_option_choice_button}/>
+      // case 3:
+      //   return 
       default:
         return <h1>stepはそのた</h1>
     }
   }
 
-  // ファイル管理
-  const [file, setFile] = useState<File|null>(null)
   
   return(
     <div>
-        {file?.name}
+        {/* {file?.name} */}
+        <Header page_title="自動名簿整形ツール"/>
         {display_content(step)}
-        <button onClick={proceedStep}>ステップをスイッチ</button>
+        {/* <button onClick={proceedStep}>ステップをスイッチ</button> */}
     </div>
   )
 }
