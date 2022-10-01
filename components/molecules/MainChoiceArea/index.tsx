b4f095d51684ccf73f5ed6742a6caebecf708bb7
kazuki <kaz.tokyo.tech20@gmail.com>
2022-10-01 20:43:29 +0900

    format

--- components/molecules/MainChoiceArea/index.tsx
+++ components/molecules/MainChoiceArea/index.tsx
@@ -7,7 +7,7 @@ import styles from "./style.module.scss";
 // 用途：オプション選択部分
 // 役割：チェックボックス選択画面の構成要素の配置
 
-interface Props{
+interface Props {
   main_action_direction: string[];
   main_button_elements: ReactElement;
   onAddressSeparaterActed: React.MouseEventHandler<HTMLButtonElement>;
@@ -17,28 +17,28 @@ interface Props{
 
 export const MainChoiceArea: Function = (props: Props): ReactElement => {
   const ComingSoonChoice: Function = (): ReactElement => {
-    return(
+    return (
       <div className={styles.choice_container}>
-        <SelectLabel text="Coming Soon ..."/>
-      </div>
-    )
-  } 
-
-  const AddressSeparaterChoice: Function = ():ReactElement => {
-    return(
-      <div className={styles.choice_container}>
-        <CheckBox onClick={props.onAddressSeparaterActed} checked={props.address_separater_selected_flag}/>
-        <SelectLabel text="住所分割"/>
+        <SelectLabel text="Coming Soon ..." />
       </div>
     )
   }
-  return(
+
+  const AddressSeparaterChoice: Function = (): ReactElement => {
+    return (
+      <div className={styles.choice_container}>
+        <CheckBox onClick={props.onAddressSeparaterActed} checked={props.address_separater_selected_flag} />
+        <SelectLabel text="住所分割" />
+      </div>
+    )
+  }
+  return (
     <div id={styles.main_choice_container}>
-      <MainDirection texts={props.main_action_direction}/>
+      <MainDirection texts={props.main_action_direction} />
       {/* todo:ここの選択要素の指定方法でうまい方法募集　現在の方法では汎用性に欠けるがギリ許容か */}
-      <AddressSeparaterChoice/>
-      <ComingSoonChoice/>
-      <ComingSoonChoice/>
+      <AddressSeparaterChoice />
+      <ComingSoonChoice />
+      <ComingSoonChoice />
       {props.main_button_elements}
     </div>
   )
