df05b5caddcd27d39d243c65b1c4701cf01d3f5b
kazuki <kaz.tokyo.tech20@gmail.com>
2022-10-01 20:41:23 +0900

    format

--- components/molecules/MainActionArea/index.tsx
+++ components/molecules/MainActionArea/index.tsx
@@ -5,16 +5,14 @@ import styles from "./style.module.scss";
 // 用途：操作指示やボタンといったメイン部分(オプション選択画面はMainChoiceArea)
 // 役割：上記部分の構成要素の配置
 
-interface Props{
+interface Props {
   main_action_direction: string[];
   main_button_elements: ReactElement;
 }
 
-export const MainActionArea: Function = (props: Props): ReactElement => {
-  return(
-    <div id={styles.main_action_container}>
-      <MainDirection texts={props.main_action_direction}/>
-      {props.main_button_elements}
-    </div>
-  )
-}
+export const MainActionArea: Function = (props: Props): ReactElement => (
+  <div id={styles.main_action_container}>
+    <MainDirection texts={props.main_action_direction} />
+    {props.main_button_elements}
+  </div>
+)
