7869ac7510b79952e3c577a26cad9124d8776d40
kazuki <kaz.tokyo.tech20@gmail.com>
2022-10-01 20:36:58 +0900

    format

--- components/atoms/SubText/index.tsx
+++ components/atoms/SubText/index.tsx
@@ -1,14 +1,10 @@
 import { ReactElement } from "react";
 import styles from "./style.module.scss";
 
-//　用途：「使い方」の説明文
-
-interface Props{
+interface Props {
   text: string;
 }
 
-export const SubText: Function = (props: Props): ReactElement => {
-  return(
-    <p id={styles.text}>{props.text}</p>
-  )
-}
+export const SubText: Function = (props: Props): ReactElement => (
+  <p id={styles.text}>{props.text}</p>
+)
