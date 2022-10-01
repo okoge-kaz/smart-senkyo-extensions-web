f3f8f89bb784ad754aa41df3eb225d9d2dfcc4e0
kazuki <kaz.tokyo.tech20@gmail.com>
2022-10-01 20:36:14 +0900

    format

--- components/atoms/SenkyoLogo/index.tsx
+++ components/atoms/SenkyoLogo/index.tsx
@@ -1,10 +1,6 @@
 import Image from "next/image"
 import { ReactElement } from "react"
 
-// 用途：ヘッダーに使う選挙のロゴ
-
-export const SenkyoLogo = (): ReactElement  => {
-  return(
-    <Image width={80} height={80} alt="センキョロゴ" src="/センキョロゴ.png" />
-  )
-}
+export const SenkyoLogo = (): ReactElement => (
+  <Image width={80} height={80} alt="センキョロゴ" src="/センキョロゴ.png" />
+)
