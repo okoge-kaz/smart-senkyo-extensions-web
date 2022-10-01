55892fea3404539626b8f2e278be45bde781acb5
kazuki <kaz.tokyo.tech20@gmail.com>
2022-10-01 20:55:16 +0900

    format

--- components/molecules/PageTitleArea/index.tsx
+++ components/molecules/PageTitleArea/index.tsx
@@ -1,20 +1,17 @@
 import { HeaderTitle } from "components/atoms/HeaderTitle";
 import { SenkyoLogo } from "components/atoms/SenkyoLogo";
-import { ReactElement } from "react";
 import styles from "./style.module.scss";
 
 // 用途：ヘッダー部分
 // 役割：ヘッダー構成要素の配置
 
-interface Props{
+interface Props {
   page_title: string;
 }
 
-export const PageTitleArea: Function = (props: Props): ReactElement => {
-  return(
-    <div id={styles.page_title_area_container}>
-      <SenkyoLogo/>
-      <HeaderTitle text={props.page_title}/>
-    </div>
-  )
-}
+export const PageTitleArea = (props: Props) => (
+  <div id={styles.page_title_area_container}>
+    <SenkyoLogo />
+    <HeaderTitle text={props.page_title} />
+  </div>
+)
