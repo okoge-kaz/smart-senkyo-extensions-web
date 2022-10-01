85df86838aa7ee841a15f96d3cd00139173f8e82
kazuki <kaz.tokyo.tech20@gmail.com>
2022-10-01 20:53:39 +0900

    format

--- components/molecules/MainChoiceArea/index.tsx
+++ components/molecules/MainChoiceArea/index.tsx
@@ -10,33 +10,30 @@ import styles from "./style.module.scss";
 interface Props {
   main_action_direction: string[];
   main_button_elements: ReactElement;
-  onAddressSeparaterActed: React.MouseEventHandler<HTMLButtonElement>;
+  onAddressSeparatorActed: React.MouseEventHandler<HTMLButtonElement>;
   address_separater_selected_flag: boolean;
 }
 
 
-export const MainChoiceArea: Function = (props: Props): ReactElement => {
-  const ComingSoonChoice: Function = (): ReactElement => {
-    return (
-      <div className={styles.choice_container}>
-        <SelectLabel text="Coming Soon ..." />
-      </div>
-    )
-  }
+export const MainChoiceArea = (props: Props) => {
+  const ComingSoonChoice = () => (
+    <div className={styles.choice_container}>
+      <SelectLabel text="Coming Soon ..." />
+    </div>
+  )
 
-  const AddressSeparaterChoice: Function = (): ReactElement => {
-    return (
-      <div className={styles.choice_container}>
-        <CheckBox onClick={props.onAddressSeparaterActed} checked={props.address_separater_selected_flag} />
-        <SelectLabel text="住所分割" />
-      </div>
-    )
-  }
+  const AddressSeparatorChoice = () => (
+    <div className={styles.choice_container}>
+      <CheckBox onClick={props.onAddressSeparatorActed} checked={props.address_separater_selected_flag} />
+      <SelectLabel text="住所分割" />
+    </div>
+  )
+
   return (
     <div id={styles.main_choice_container}>
       <MainDirection texts={props.main_action_direction} />
       {/* todo:ここの選択要素の指定方法でうまい方法募集　現在の方法では汎用性に欠けるがギリ許容か */}
-      <AddressSeparaterChoice />
+      <AddressSeparatorChoice />
       <ComingSoonChoice />
       <ComingSoonChoice />
       {props.main_button_elements}
