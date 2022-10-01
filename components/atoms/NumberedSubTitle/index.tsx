f12789504d2a34c2a994b789d2dec6dfbc5d5ff6
kazuki <kaz.tokyo.tech20@gmail.com>
2022-10-01 20:30:45 +0900

    format

--- components/atoms/NumberedSubTitle/index.tsx
+++ components/atoms/NumberedSubTitle/index.tsx
@@ -3,16 +3,15 @@ import styles from "./style.module.scss";
 
 // 用途：「使い方」の各手順のタイトルのように数字付きのサブタイトル
 
-interface Props{
+interface Props {
   number: number;
   text: string;
 }
 
-export const NumberedSubTitle: Function = (props: Props): ReactElement =>{
-  return(
-    <div id={styles.title_container}>
-      <p id={styles.title_number}>{props.number}</p>
-      <p id={styles.title}>{props.text}</p>
-    </div>
-  )
-}
+export const NumberedSubTitle: Function = (props: Props): ReactElement => (
+  <div id={styles.title_container}>
+    <p id={styles.title_number}>{props.number}</p>
+    <p id={styles.title}>{props.text}</p>
+  </div>
+)
+
