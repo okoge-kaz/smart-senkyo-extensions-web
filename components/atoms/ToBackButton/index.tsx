070630c6fbbf03adfd9154e81cbed70652f19e11
kazuki <kaz.tokyo.tech20@gmail.com>
2022-10-01 20:37:49 +0900

    format

--- components/atoms/ToBackButton/index.tsx
+++ components/atoms/ToBackButton/index.tsx
@@ -3,15 +3,15 @@ import styles from "./style.module.scss";
 // 用途：ファイルアップロード等の特殊なことがない、通常の戻るボタン
 // 役割：前の画面への遷移(onClick)
 
-interface Props{
+interface Props {
   label: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export const ToBackButton = (props: Props) =>{
-  return(
-    <div id={styles.container}>
-      <button id={styles.button} onClick={props.onClick}>{props.label}</button>
-    </div>
-  )
-}
+export const ToBackButton = (props: Props) => (
+  <div id={styles.container}>
+    <button id={styles.button} onClick={props.onClick}>
+      {props.label}
+    </button>
+  </div>
+)
