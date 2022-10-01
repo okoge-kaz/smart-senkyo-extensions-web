5e521c524c2a34fb8061dd6a5519fdb111e0ce3a
kazuki <kaz.tokyo.tech20@gmail.com>
2022-10-01 20:56:23 +0900

    format

--- components/organisms/Hreader/index.tsx
+++ components/organisms/Hreader/index.tsx
@@ -4,12 +4,10 @@ import { ReactElement } from "react";
 // 用途：ヘッダー部分
 // 役割：引数をそのまま流すだけ// todo:このorganismsいる？
 
-interface Props{
+interface Props {
   page_title: string;
 }
 
-export const Header: Function = (props: Props): ReactElement => {
-  return(
-    <PageTitleArea page_title={props.page_title}/>
-  )
-}
+export const Header: Function = (props: Props): ReactElement => (
+  <PageTitleArea page_title={props.page_title} />
+)
