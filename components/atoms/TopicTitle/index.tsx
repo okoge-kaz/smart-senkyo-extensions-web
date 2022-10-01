2864fa630427d968024fca0b628f6364f6200d5f
kazuki <kaz.tokyo.tech20@gmail.com>
2022-10-01 20:38:52 +0900

    format

--- components/atoms/TopicTitle/index.tsx
+++ components/atoms/TopicTitle/index.tsx
@@ -3,12 +3,10 @@ import styles from "./style.module.scss";
 
 // 用途：「使い方」のタイトル等項目ごとのタイトル
 
-interface Props{
+interface Props {
   text: string;
 }
 
-export const TopicTitle: Function = (props: Props): ReactElement => {
-  return(
-    <p id={styles.topic_title}>{props.text}</p>
-  )
-}
+export const TopicTitle: Function = (props: Props): ReactElement => (
+  <p id={styles.topic_title}>{props.text}</p>
+)
