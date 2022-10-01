d9490824ff008bd732b05884bde215090dd7cd86
kazuki <kaz.tokyo.tech20@gmail.com>
2022-10-01 20:29:00 +0900

    format

--- components/atoms/DownloadButton/index.tsx
+++ components/atoms/DownloadButton/index.tsx
@@ -1,22 +1,24 @@
-import type { ReactElement } from "react";
-import styles from "./style.module.scss";
+import type { ReactElement } from "react"
+import styles from "./style.module.scss"
 
-interface Props{
-  label: string;
-  onActed: Function;
-  setFile: Function;
+interface Props {
+  label: string
+  onActed: Function
+  setFile: Function
 }
 
-export const DownloadButton: Function = (props: Props): ReactElement =>{
+export const DownloadButton: Function = (props: Props): ReactElement => {
   const handleFileImportChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) props.setFile(e.target.files) // File
-    props.onActed();
+    props.onActed()
   };
-  return(
+  return (
     <div id={styles.upload_container}>
       {/* todo:idにまとめて書きたいが{styles.~}と"~"の併用方法がわからない */}
-      <input className={styles.upload_input} id="upload_input" type='file' onChange={handleFileImportChange} multiple/>
-      <label htmlFor="upload_input" id={styles.upload_label}>{props.label}</label>
+      <input className={styles.upload_input} id="upload_input" type='file' onChange={handleFileImportChange} multiple />
+      <label htmlFor="upload_input" id={styles.upload_label}>
+        {props.label}
+      </label>
     </div>
   )
 }
