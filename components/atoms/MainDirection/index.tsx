644fb6ac10b18be5928328f5ce03970b2f860f55
kazuki <kaz.tokyo.tech20@gmail.com>
2022-10-01 20:27:26 +0900

    format

--- components/atoms/MainDirection/index.tsx
+++ components/atoms/MainDirection/index.tsx
@@ -1,19 +1,19 @@
-import { ReactElement } from 'react';
-import styles from './style.module.scss';
+import { ReactElement } from 'react'
+import styles from './style.module.scss'
 
 // 用途：画面中央のメインの指示の文章
 // 役割：表示したい文字を["abc", "foo", ...]と受け取り要素ごとに改行して表示
 
-interface Props{
-  texts: string[];
+interface Props {
+  texts: string[]
 }
 
-export const MainDirection = (props: Props): ReactElement  =>  (
+export const MainDirection = (props: Props): ReactElement => (
   <div id={styles.main_direction_container}>
     {props.texts.map((text, index) =>
-    <p key={index} className={styles.main_direction}>
-      {text}
-    </p>
-  )}
+      <p key={index} className={styles.main_direction}>
+        {text}
+      </p>
+    )}
   </div>
 )
