f4e8ef02076ed6692599a6ea993b8cd03b01f868
gray-1to <graystin88@gmail.com>
2022-10-03 00:18:40 +0900

    set favicon and rename senkyo-logo

--- components/atoms/SenkyoLogo/index.tsx
+++ components/atoms/SenkyoLogo/index.tsx
@@ -2,5 +2,5 @@ import Image from "next/image"
 import React from "react"
 
 export const SenkyoLogo = React.memo(() => (
-  <Image width={80} height={80} alt="センキョロゴ" src="/センキョロゴ.png" />
+  <Image width={80} height={80} alt="センキョのロゴ" src="/senkyo-logo.png" />
 ))
