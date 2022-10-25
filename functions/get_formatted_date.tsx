8fcc300e91bca462d97d8d7902e4162254da9378
gray-1to <graystin88@gmail.com>
2022-10-25 23:57:50 +0900

    rename args and revise date in get_formatted_date

--- functions/get_formatted_date.tsx
+++ functions/get_formatted_date.tsx
@@ -1,15 +1,15 @@
-export default function get_formatted_date(date: Date): string{
-  // args: Date
-  // return: request_time: 2022-10-25 23:48:19Zのような形式になる
+export default function get_formatted_date(date_data: Date): string{
+  // args: date_data
+  // return: request_time: 2022-10-25 23:56.14Zのような形式になる
   // 
   // APIに付属させるリクエスト日時の情報の生成
 
-  const year: string = date.getFullYear().toString();
-  const month: string = (date.getMonth() + 1).toString(); //getMonthでは1月が0になる
-  const day: string = date.getDay().toString();
-  const hour: string = date.getHours().toString();
-  const minute: string = date.getMinutes().toString();
-  const second: string = date.getSeconds().toString();
-  const request_time: string = `${year}-${month}-${day} ${hour}:${minute}.${second}Z`
+  const year: string = date_data.getFullYear().toString();
+  const month: string = (date_data.getMonth() + 1).toString(); //getMonthでは1月が0になる
+  const date: string = date_data.getDate().toString();
+  const hour: string = date_data.getHours().toString();
+  const minute: string = date_data.getMinutes().toString();
+  const second: string = date_data.getSeconds().toString();
+  const request_time: string = `${year}-${month}-${date} ${hour}:${minute}.${second}Z`
   return request_time;
 }
