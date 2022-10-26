19af20e5f27914e4b7980c1a0df9531de062a6a9
kazuki <kaz.tokyo.tech20@gmail.com>
2022-10-26 23:47:02 +0900

    change into arrow function and format

--- functions/read_file_list.tsx
+++ functions/read_file_list.tsx
@@ -1,22 +1,32 @@
 import { read, utils } from "xlsx";
 
-export default async function read_file_list(index: number, files_list: File[], file_reader: FileReader, file_names: string[], sheet_names: string[], json_formed_sheets: Array<JSON>, file_state_length: number, after_roop_function: Function, error_handle_function: Function){
-  // args: index: 読み込みたいファイルのfile_listでのインデックス
-  //       files_list:
-  //       file_reader: FileReaderを使い回すために引数として与えている
-  //       file_names: 
-  //       sheet_names:
-  //       json_formed_sheets: ファイルをJSON化した後に格納する配列
-  //       file_state_length: files_listの長さ
-  //       after_roop_function: 全ファイルを読み取った後に実行される関数
-  //       error_handle_function: indexがfiles_list外を示す時に実行される関数
-  // return: void
-  // 
-  // 自身を再起的に呼び出し非同期であるFileReader.readAsArrayBuffer()でファイルを順に読み込むための関数
+const read_file_list = async (
+  index: number,
+  files_list: File[], file_reader: FileReader, file_names: string[],
+  sheet_names: string[], json_formed_sheets: Array<JSON>,
+  file_state_length: number, after_loop_function: Function, error_handle_function: Function) => {
+  /*
+  Arguments:
+    index: number                     読み込みたいファイルのfile_listでのインデックス
+    files_list: File[]
+    file_reader: FileReader           FileReaderを使い回すために引数として与えている
+    file_names: string[]
+    sheet_names: string[]
+    json_formed_sheets: Array<JSON>   ファイルをJSON化した後に格納する配列
+    file_state_length: number         files_listの長さ
+    after_loop_function: Function     全ファイルを読み取った後に実行される関数
+    error_handle_function: Function   indexがfiles_list外を示す時に実行される関数
+
+  Returns:
+    void
+
+  Description:
+    自身を再起的に呼び出し非同期であるFileReader.readAsArrayBuffer()でファイルを順に読み込むための関数
+  */
   const file = files_list[index];
-  file_reader.onload = (event) =>{
+  file_reader.onload = (event) => {
     // まだ読み取るファイルがあるなら再帰的に呼び出す
-    if(index < file_state_length){
+    if (index < file_state_length) {
       // file_readerの読み込み結果
       const result = event.target?.result;
       const workbook = read(result, { type: "array" });
@@ -25,14 +35,16 @@ export default async function read_file_list(index: number, files_list: File[], 
       sheet_names[index] = workbook.SheetNames[0];
       const worksheet = workbook.Sheets[workbook.SheetNames[0]];
       json_formed_sheets[index] = utils.sheet_to_json(worksheet);
-      if(index == file_state_length - 1){
-        after_roop_function();
-      }else{
-        read_file_list(index+1, files_list, file_reader, file_names, sheet_names, json_formed_sheets, file_state_length, after_roop_function, error_handle_function);
+      if (index == file_state_length - 1) {
+        after_loop_function();
+      } else {
+        read_file_list(index + 1, files_list, file_reader, file_names, sheet_names, json_formed_sheets, file_state_length, after_loop_function, error_handle_function);
       }
-    }else{
+    } else {
       error_handle_function();
     }
   }
   file_reader.readAsArrayBuffer(file);// file_reader.readAsArrayBuffer()は非同期なので注意
 }
+
+export default read_file_list
